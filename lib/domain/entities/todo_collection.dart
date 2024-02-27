cff995e792ec6ea3b9479a21457fb65feb7f8509
fabioychinen <astoufo_chinen@hotmail.com>
2024-02-27 17:06:50 -0300

    Task T03

---
+++ lib/domain/entities/todo_collection.dart
@@ -0,0 +1,20 @@
+import 'package:todo_app/domain/entities/todo_color.dart';
+import 'package:todo_app/domain/entities/unique_id.dart';
+
+class ToDoCollection {
+  final CollectionId id;
+  final String title;
+  final ToDoColor color;
+  ToDoCollection({
+    required this.id,
+    required this.title,
+    required this.color,
+  });
+  factory ToDoCollection.empty() {
+    return ToDoCollection(
+      id: CollectionId(),
+      title: '',
+      color: ToDoColor(colorIndex: 0),
+    );
+  }
+}
